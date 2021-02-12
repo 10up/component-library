96b26a2d737456bf9e1f82c75bb90ef4df2a64e7
Nícholas André <nicholas.andre@10up.com>
2021-02-11 22:59:55 -0300

    Updating countdown timer tests

--- packages/countdown-timer/src/__tests__/countdown-timer.js
+++ packages/countdown-timer/src/__tests__/countdown-timer.js
@@ -1,21 +1,30 @@
 import { screen } from '@testing-library/dom';
 import { axe } from 'jest-axe';
+import { render } from 'test-utils/dom';
 import { CountdownTimer } from '..';
+
+let globalContainer;
 
 beforeEach(() => {
 	const oneYearFromNow = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
 
-	document.body.innerHTML = `
+	const { container } = render(`
 	<time class="countdown-timer" datetime="${oneYearFromNow.toISOString()}">
 		${oneYearFromNow.toISOString()}
 	</time>
-	`;
+	`);
+
+	globalContainer = container;
 });
 
-test.skip('markup is accessible', async () => {
+afterEach(() => {
+	document.body.removeChild(globalContainer);
+});
+
+test('markup is accessible', async () => {
 	new CountdownTimer('.countdown-timer');
 
-	expect(await axe(document.body.innerHTML)).toHaveNoViolations();
+	expect(await axe(document.querySelector('.countdown-timer'))).toHaveNoViolations();
 });
 
 test('countdown timer starts a timer', () => {
