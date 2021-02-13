4af66737d1d603bd217a474fac5bcbd3fc8266bf
Nícholas André <nicholas.andre@10up.com>
2021-02-13 09:00:27 -0300

    Code Coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	setupFilesAfterEnv: ['./setupTests.js'],
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(j|t)sx?$',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+	moduleFileExtensions: ['js', 'jsx'],
 	testPathIgnorePatterns: ['/node_modules/', '/mocks/'],
 	moduleNameMapper: {
 		'\\.css$': require.resolve('./test-utils/style-mock'),
