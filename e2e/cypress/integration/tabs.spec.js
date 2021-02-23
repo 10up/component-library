7d2a2d15f735e8f399e77d4645b3476b576568cd
Nícholas André <nicholas.andre@10up.com>
2021-02-22 21:41:53 -0300

    Adding CHANGELOG.md and cypress tests

--- e2e/cypress/integration/tabs.spec.js
+++ e2e/cypress/integration/tabs.spec.js
@@ -36,4 +36,13 @@ describe('tabs', () => {
 		cy.findByText('View Tab 4').click();
 		cy.findByText('Vertical Tab 4 Content').should('be.visible');
 	});
+
+	it('maitains focusable elements within the current tab', () => {
+		cy.findByText('View Vertical Tab 1').click();
+		// ensures there's only one tabpanel visible and it can find a link by role
+		cy.get('.tabs').findByRole('tabpanel').findByRole('link');
+
+		// links within the second tab should not be acessible
+		cy.get('.tabs #js-tab2').findByRole('link').should('not.exist');
+	});
 });
