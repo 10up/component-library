4f5583c6989a271b11d96f15e4aae1df1d7483ec
Nícholas André <nicholas.andre@10up.com>
2021-04-22 18:09:10 -0300

    Basic ts defs

---
+++ packages/reading-position/index.d.ts
@@ -0,0 +1,35 @@
+declare module '@10up/reading-position' {
+	type ReadingPositionOptions = {
+		/**
+		 * Called after the reading position is initialized on page load.
+		 */
+		onCreate?: () => void,
+
+		/**
+		 * Called when an accordion item is opened
+		 */
+		scrollStart?: () => void,
+
+		/**
+		 * Called when an accordion item is closed
+		 */
+		scrolling?: () => void,
+
+		/**
+		 * Called when an accordion item is toggled
+		 */
+		scrollEnd?: () => void,
+	}
+
+	export type DestroyOptions = {
+		/**
+		 * Remove all custom attributes added by the component
+		 */
+		removeAttributes: boolean,
+	}
+
+	export class ReadingPosition {
+		constructor(element: string, options?: ReadingPositionOptions);
+		destroy(options?: DestroyOptions);
+	}
+}
