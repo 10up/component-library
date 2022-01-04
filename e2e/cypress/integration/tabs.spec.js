c20adb7da27916d7ad0b65840294564003f32d08
Alex Dimitrov <alex.dimitrov@10up.com>
2022-01-04 14:25:31 +0200

    Add cypress tests for nested tabs support

--- e2e/cypress/integration/tabs.spec.js
+++ e2e/cypress/integration/tabs.spec.js
@@ -37,6 +37,43 @@ describe('tabs', () => {
 		cy.findByText('Vertical Tab 4 Content').should('be.visible');
 	});
 
+	it('opens and close Tab Nested Parent properly', () => {
+		cy.findByText('View Nested Parent Tab 1').click();
+		cy.findByText('Tab 1 Content (nested tab inside)').should('be.visible');
+
+		cy.findByText('View Nested Parent Tab 2').click();
+		cy.findByText('Tab Nested Parent 2 Content').should('be.visible');
+
+		cy.findByText('View Nested Parent Tab 3').click();
+		cy.findByText('Tab Nested Parent 3 Content').should('be.visible');
+
+		cy.findByText('View Nested Parent Tab 4').click();
+		cy.findByText('Tab Nested Parent 4 Content').should('be.visible');
+
+		cy.findByText('View Nested Parent Tab 5').click();
+		cy.findByText('Tab Nested Parent 5 Content').should('be.visible');
+	});
+
+	it('opens and close Tab Nested properly', () => {
+		cy.findByText('View Nested Parent Tab 1').click();
+		cy.findByText('Tab 1 Content (nested tab inside)').should('be.visible');
+
+		cy.findByText('View Nested Tab 1').click();
+		cy.findByText('Nested Tab 1 Content').should('be.visible');
+
+		cy.findByText('View Nested Tab 2').click();
+		cy.findByText('Nested Tab 2 Content').should('be.visible');
+
+		cy.findByText('View Nested Tab 3').click();
+		cy.findByText('Nested Tab 3 Content').should('be.visible');
+
+		cy.findByText('View Nested Tab 4').click();
+		cy.findByText('Nested Tab 4 Content').should('be.visible');
+
+		cy.findByText('View Nested Tab 5').click();
+		cy.findByText('Nested Tab 5 Content').should('be.visible');
+	});
+
 	it('maitains focusable elements within the current tab', () => {
 		cy.findByText('View Vertical Tab 1').click();
 		// ensures there's only one tabpanel visible and it can find a link by role
