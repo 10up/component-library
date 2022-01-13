3389e7aa8233ea50da023fa6e3f42319b4427d23
Ajay Maurya <ajaymaurya@Ajays-MacBook-Pro.local>
2022-01-13 23:26:54 +0530

    SubMenu Item: Add to onSubmenuClose callback

--- packages/navigation/src/navigation.js
+++ packages/navigation/src/navigation.js
@@ -337,7 +337,7 @@ export default class Navigation {
 		 * @callback onSubmenuClose - optional.
 		 */
 		if (this.settings.onSubmenuClose && typeof this.settings.onSubmenuClose === 'function') {
-			this.settings.onSubmenuClose.call();
+			this.settings.onSubmenuClose.call(this, $submenu);
 		}
 	}
 
